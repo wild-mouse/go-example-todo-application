7fcc215a476024791cf55c13951af062925167dd
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-28 11:52:09 +0900

    Add getting task record from database

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
@@ -13,18 +14,32 @@ type Task struct {
 	Name string `json:"name"`
 }
 
-func tasksHandler(w http.ResponseWriter, r *http.Request) {
+func makeHandler(fn func(http.ResponseWriter, *http.Request, *sql.DB), db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fn(w, r, db)
+	}
+}
+
+func tasksHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if r.Method == http.MethodGet {
-		fmt.Println("Handling GET Method")
-		task := &Task{1, "hogehgoe"}
+		row := db.QueryRow("SELECT * FROM tasks WHERE id=1") //" WHERE id=1")
+		var task Task
+		err := row.Scan(&task.Id, &task.Name)
+		if err != nil {
+			fmt.Println(err)
+		}
 		bytes, _ := json.Marshal(task)
-		_, err := fmt.Fprint(w, string(bytes))
+		_, err = fmt.Fprint(w, string(bytes))
 		if err != nil {
 			fmt.Println(err)
 		}
 	}
 	if r.Method == http.MethodPost {
 		fmt.Println("Handling POST Method")
+		_, err := db.Query("insert into tasks values (1, \"hogehoge\")")
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 	if r.Method == http.MethodPut {
 		fmt.Println("Handling PUT Method")
@@ -35,14 +50,10 @@ func tasksHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	//db, err := sql.Open("mysql", "docker:docker@/playground")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//_, err = db.Query("insert into tasks values (1, \"hogehoge\")")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	http.HandleFunc("/tasks", tasksHandler)
+	db, err := sql.Open("mysql", "docker:docker@/playground")
+	if err != nil {
+		fmt.Println(err)
+	}
+	http.HandleFunc("/tasks", makeHandler(tasksHandler, db))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
