c73828b7ecadbf057862174a015a6e5756f9e27d
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-28 10:36:27 +0900

    Add connecting to mysql

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	//"github.com/gin-gonic/gin"
+	"database/sql"
+	"fmt"
+	//"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	//"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	db, err := sql.Open("mysql", "docker:docker@/playground")
+	if err != nil {
+		fmt.Println(err)
+	}
+	_, err = db.Query("insert into tasks values (1, \"hogehoge\")")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//r := gin.Default()
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "pong",
+	//	})
+	//})
+	//r.Run()
 }
