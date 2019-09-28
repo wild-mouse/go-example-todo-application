3110837e0a5e7aeeb0e326d73f12ad0fbe5e2010
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-28 10:59:45 +0900

    Add handling methods

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
-	//"github.com/gin-gonic/gin"
-	"database/sql"
 	"fmt"
-	//"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	//"net/http"
+	"log"
+	"net/http"
 )
 
+type Task struct {
+	Id uint32
+	Name string
+}
+
+func tasksHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Println("Handling GET Method")
+	}
+	if r.Method == http.MethodPost {
+		fmt.Println("Handling POST Method")
+	}
+	if r.Method == http.MethodPut {
+		fmt.Println("Handling PUT Method")
+	}
+	if r.Method == http.MethodDelete {
+		fmt.Println("Handling DELETE Method")
+	}
+}
+
 func main() {
-	db, err := sql.Open("mysql", "docker:docker@/playground")
-	if err != nil {
-		fmt.Println(err)
-	}
-	_, err = db.Query("insert into tasks values (1, \"hogehoge\")")
-	if err != nil {
-		fmt.Println(err)
-	}
+	//db, err := sql.Open("mysql", "docker:docker@/playground")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//_, err = db.Query("insert into tasks values (1, \"hogehoge\")")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	http.HandleFunc("/tasks", tasksHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 	//r := gin.Default()
 	//r.GET("/ping", func(c *gin.Context) {
