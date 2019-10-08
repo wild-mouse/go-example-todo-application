0d8eaf74abfbf0bfd29652e558c629e474b587e1
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-10-08 19:52:26 +0900

    Add swagger document and swagger-ui

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -3,17 +3,32 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/go-chi/chi"
 	_ "github.com/go-sql-driver/mysql"
+	httpSwagger "github.com/swaggo/http-swagger"
+	_ "github.com/wild-mouse/go-example-todo-application/docs"
+	"github.com/wild-mouse/go-example-todo-application/pkg/handlers"
 	"log"
 	"net/http"
-	"github.com/wild-mouse/go-example-todo-application/pkg/handlers"
 )
 
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService
 func main() {
 	db, err := sql.Open("mysql", "docker:docker@/playground")
 	if err != nil {
 		fmt.Println(err)
 	}
 	http.HandleFunc("/tasks/", handlers.MakeHandler(handlers.TasksHandler, db))
+
+	r := chi.NewRouter()
+
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:1323/swagger/doc.json"),
+	))
+	http.ListenAndServe(":1323", r)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/wild-mouse/go-example-todo-application
 go 1.13
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/golang/protobuf v1.3.2
 	github.com/gorilla/websocket v1.4.1 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0
-	github.com/opentracing/opentracing-go v1.1.0
+	github.com/swaggo/http-swagger v0.0.0-20190614090009-c2865af9083e
+	github.com/swaggo/swag v1.6.3
 	goa.design/goa/v3 v3.0.6
 	golang.org/x/net v0.0.0-20190930134127-c5a3c61f89f3 // indirect
 	golang.org/x/sys v0.0.0-20190927073244-c990c680b611 // indirect
